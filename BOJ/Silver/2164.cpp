a0b9278f81573f81687dbd3e5b2ef25f61b3caa6
leebs0521 <leebs0521@gmail.com>
2021-09-29 15:55:04 +0900

    BOJ 2798 블랙잭

--- BOJ/Silver/2164.cpp
+++ BOJ/Silver/2164.cpp
@@ -9,8 +9,11 @@
 #include <deque>
 using namespace std;
 
-int main(){
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
     int n, phase = 0;
     deque<int> d;
@@ -18,22 +21,22 @@ int main(){
     cin >> n;
 
     // 1 ~ N 까지 deque push
-    for(int i=1;i<=n;i++)
+    for (int i = 1; i <= n; i++)
         d.push_back(i);
-    
 
     // 카드가 하나만 남을 때 까지
-    while(d.size() != 1){
+    while (d.size() != 1)
+    {
         int item;
         phase++;
-        
+
         // 가장 앞에 있는 숫자 pop
         item = d.front();
         d.pop_front();
 
         // 짝수 페이즈에 가장 앞에 있는 숫자를 뒤로 보내므로
         // push back
-        if(phase%2 == 0)
+        if (phase % 2 == 0)
             d.push_back(item);
     }
 
