eb32f48aa8703ffa6a4243d3fdf7c840ede3a8c9
leebs0521 <leebs0521@gmmail.com>
2022-04-08 23:25:36 +0900

    .

---
+++ BOJ/Bronze/10926.cpp
@@ -0,0 +1,24 @@
+/*
+    BOJ 10926 - ??!
+    url: https://www.acmicpc.net/problem/10926
+    Bronze 5
+    date: 2022. 4. 8
+*/
+
+#include <iostream>
+
+#define endl "\n"
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    string str;
+    
+    cin >> str;
+    
+    cout << str << "??!" << endl;
+    return 0;
+}
+
