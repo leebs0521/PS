8c8482894469a168a74577dcbd429540ae6a6a4f
leebs0521 <leebs0521@gmail.com>
2022-08-13 22:05:11 +0900

    2022. 8. 13

---
+++ BOJ/Bronze/2558.cpp
@@ -0,0 +1,23 @@
+/*
+    BOJ 2558 - A+B - 2
+    url: https://www.acmicpc.net/problem/2558
+    Bronze 5
+    date: 2022. 8. 13
+*/
+
+#include <iostream>
+
+#define endl "\n"
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << a + b;
+    return 0;
+}
